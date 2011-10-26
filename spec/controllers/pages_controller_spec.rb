865b1814e6877bce711cef22ea2be59ec3d3bfed
Ilia Formalsky <formal777@gmail.com>
2011-10-26 22:03:46 +0400

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,7 +9,7 @@ describe PagesController do
       response.should be_success
     end
 
-    it "should be rite title" do
+    it "should be write title" do
       get 'home'
       response.should have_selector "title", :content => "Ruby on Rails Tutorial Sample App | Home"
     end
@@ -21,7 +21,7 @@ describe PagesController do
       response.should be_success
     end
 
-    it "should be rite title" do
+    it "should be write title" do
       get 'contact'
       response.should have_selector "title", :content => "Ruby on Rails Tutorial Sample App | Contact"
     end
@@ -33,7 +33,7 @@ describe PagesController do
       response.should be_success
     end
 
-    it "should be rite title" do
+    it "should be write title" do
       get 'about'
       response.should have_selector "title", :content => "Ruby on Rails Tutorial Sample App | About"
     end
